8ad919adedfaa9d7b6873bff5545bbb51d07f11f
Antoine COULON <a.antoinecoulon@gmail.com>
2025-04-25 15:44:52 +0200

    Add logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,13 @@
 import Navbar from "./Navbar"
+import logo from "../../assets/logo-temp.svg"
 
 function Header() {
     return (
-        <header className="w-full flex justify-between">
-            <h1>Portfolio</h1>
+        <header className="w-full flex justify-between items-center p-4 shadow-[0_3px_10px_rgb(0,0,0,0.3)]">
+            <div className="logo-block flex items-center gap-4">
+                <img src={logo} className="w-[80px]" alt="Logo temporaire" />
+                <h1>Portfolio d'Antoine COULON</h1>
+            </div>
             <Navbar />
         </header>
     )
