5156126fd9086172328de18fc6a6fba063d36d7c
Antoine COULON <a.antoinecoulon@gmail.com>
2025-04-25 15:45:31 +0200

    Add image placeholder + up text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 
 function Home() {
     return (
-        <main className='w-full h-full flex justify-center items-center gap-8'>
-            <div className='home-illustration w-[500px] h-[500px] bg-accent-primary'>
-
+        <main className='w-full h-full flex justify-center items-center gap-8  font-light'>
+            <div className='home-illustration w-[500px] h-[500px] bg-accent-primary p-2'>
+                <img src="https://picsum.photos/540" alt="illustration aléatoire" />
             </div>
             <div className="home-text w-[500px]">
                 <h2>Hello from Home!</h2>
-                <p>Bienvenue sur mon portfolio !!!</p>
+                <p className="text-justify">
+                    Bienvenue sur mon portfolio !!! <br/>
+                    Le site est en construction actuellement et sera bientôt opérationnel ! Il me permettra d'exposer mes projets personnels et l'évolution de mon apprentissage. Actuellement en formation, je ferai évoluer ce portfolio au même rythme que moi-même. J'ai construit ce site en utilisant <span className="text-highlight">React</span> avec Vite, et Tailwind.
+                </p>
             </div>
         </main>
     )
