d52787e32397030665514aa3261247d7716d2bd2
Antoine COULON <a.antoinecoulon@gmail.com>
2025-04-26 22:40:07 +0200

    Timeline

---
+++ src/data/experiences.js
@@ -0,0 +1,13 @@
+export const experiences = [
+  {
+    year: "2015",
+    experience: "BTS Informatique",
+    description:
+      "Les bases du développement web et de l'administration systèmes et réseaux.",
+  },
+  {
+    year: "2025",
+    experience: "Titre Concepteur Développeur d'Applications Web et Mobile",
+    description: "Titre professionnel certifié par l'Etat de niveau 6, axé sur le développement d'applications web et mobile, l'analyse et la conception d'applications et la gestion de projet."
+  }
+];
--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -1,5 +1,32 @@
+import { experiences } from "../data/experiences";
+
+/*
+
+    TODO:
+
+    Animer les points quand on scrolle (Framer Motion)
+    Ajouter des icones (diplome, stage, certificat)
+    Ajouter les certificats
+    Verticale sur mobile, horizontale sur écran large
+
+*/
+
+
 export default function Timeline() {
-    return (
-        <h1>Hello from Timeline!</h1>
-    )
+  return (
+    <section className="timeline relative p-10">
+      <div className="timeline-frise absolute left-1/2 transform -translate-x-1/2 h-full w-1 bg-highlight"></div>
+      <div className="timeline-items space-y-12">
+        {experiences.map((exp, index) => (
+          <div key={index} className={`timeline-item flex items-center w-full ${index % 2 === 0 ? 'justify-start' : 'justify-end'}`}>
+            <div className="w-5/12 p-4 bg-white rounded-lg shadow-md">
+                <h3 className="text-lg font-bold text-highlight">{exp.experience}</h3>
+                <span className="text-sm font-semibold text-accent-secondary">{exp.year}</span>
+                <p className="text-sm text-gray-600">{exp.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
