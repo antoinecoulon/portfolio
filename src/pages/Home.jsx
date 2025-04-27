6d60c0d5782eb43925114b099a580929176b1ff4
Antoine COULON <a.antoinecoulon@gmail.com>
2025-04-27 22:36:02 +0200

    Home v1 ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,25 @@
+import "./Home.css";
+export default function Home() {
+  return (
+    <main>
+      <div className="box">
+        <span className="letter">A</span>
+        <span className="letter">N</span>
+        <span className="letter">T</span>
+        <span className="letter">O</span>
+        <span className="letter">I</span>
+        <span className="letter">N</span>
+        <span className="letter">E</span>
+      </div>
 
-function Home() {
-    return (
-        <main className='w-full h-full flex justify-center items-center gap-8  font-light'>
-            <div className='home-illustration w-[500px] h-[500px] bg-accent-primary p-2'>
-                <img src="https://picsum.photos/540" alt="illustration aléatoire" />
-            </div>
-            <div className="home-text w-[500px]">
-                <h2>Hello from Home!</h2>
-                <p className="text-justify">
-                    Bienvenue sur mon portfolio !!! <br/>
-                    Le site est en construction actuellement et sera bientôt opérationnel ! Il me permettra d'exposer mes projets personnels et l'évolution de mon apprentissage. Actuellement en formation, je ferai évoluer ce portfolio au même rythme que moi-même. J'ai construit ce site en utilisant <span className="text-highlight">React</span> avec Vite, et Tailwind.
-                </p>
-            </div>
-        </main>
-    )
+      <div className="box">
+        <span className="letter">C</span>
+        <span className="letter">O</span>
+        <span className="letter">U</span>
+        <span className="letter">L</span>
+        <span className="letter">O</span>
+        <span className="letter">N</span>
+      </div>
+    </main>
+  );
 }
-
-export default Home
