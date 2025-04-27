ecb2ed440c6574197982f117eeed8f27feb735b0
Antoine COULON <a.antoinecoulon@gmail.com>
2025-04-27 20:51:40 +0200

    Fix: delete Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  base: './'
-})
+  plugins: [react()],
+  base: "./",
+});
