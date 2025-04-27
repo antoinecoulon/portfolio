af9c82c4ef0786ecbb4d5bef18db4d58dca23d8a
Antoine COULON <a.antoinecoulon@gmail.com>
2025-04-27 21:57:59 +0200

    Fix CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
+import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
-import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
