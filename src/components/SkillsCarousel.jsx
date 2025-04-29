8212b95cc6e60c48c6e08e8d64df1ed667939393
Antoine COULON <a.antoinecoulon@gmail.com>
2025-04-29 10:32:48 +0200

    Fix CSS + finish page template

--- src/components/SkillsCarousel.jsx
+++ src/components/SkillsCarousel.jsx
@@ -1,24 +1,25 @@
+import React from "react"
 import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
 import './SkillsCarousel.css'
 
+const carousel = (slider) => {
+  const z = 300
+  function rotate() {
+    const deg = 360 * slider.track.details.progress
+    slider.container.style.transform = `translateZ(-${z}px) rotateY(${-deg}deg)`
+  }
+  slider.on("created", () => {
+    const deg = 360 / slider.slides.length
+    slider.slides.forEach((element, idx) => {
+      element.style.transform = `rotateY(${deg * idx}deg) translateZ(${z}px)`
+    })
+    rotate()
+  })
+  slider.on("detailsChanged", rotate)
+}
+
 export default function SkillsCarousel({ skills }) {
-    
-    const carousel = (slider) => {
-        const z = 300
-        function rotate() {
-            const deg = 360 * slider.track.details.progress
-            slider.container.style.transform = `translateZ(-${z}px) rotateY(${-deg}deg)`
-        }
-        slider.on("created", () => {
-            const deg = 360 / slider.slides.length
-            slider.slides.forEach((element, index) => {
-                element.style.transform = `rotateY(${deg * index}deg) translateZ(${z}px`
-            })
-            rotate()
-        })
-        slider.on("detailsChanged", rotate)
-    }
     
     const [sliderRef] = useKeenSlider(
         {
@@ -35,9 +36,16 @@ export default function SkillsCarousel({ skills }) {
       <div className="scene">
         <div className="carousel keen-slider" ref={sliderRef}>
           {skills.map((skill, index) => (
-            <div key={index} className="carousel__cell">
-              <img src={skill.logo} alt={skill.name} className="w-12 h-12" />
-              <p className="text-sm mt-2">{skill.name}</p>
+            <div 
+              key={index} 
+              className={
+                skill.isLearning 
+                  ? `carousel__cell slide isLearning`
+                  : `carousel__cell slide`
+              }
+            >
+              <img className="skill-logo" src={skill.logo} alt={skill.name} />
+              <p className="skill-name">{skill.name}</p>
             </div>
           ))}
         </div>
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,18 +1,41 @@
 import SkillsCarousel from "../components/SkillsCarousel"
+import './Skills.css'
+
+import htmlLogo from "../assets/skills-icons/html5.svg";
+import cssLogo from "../assets/skills-icons/css.svg";
+import jsLogo from "../assets/skills-icons/javascript.svg";
+import reactLogo from "../assets/skills-icons/react_dark.svg";
+import nodeLogo from "../assets/skills-icons/nodejs.svg";
+
 
 export default function Skills() {
-    const learning = {name: "React.js", logo: ""}
     const skills = [
-        {name: "HTML", logo: ""},
-        {name: "CSS", logo: ""},
-        {name: "JavaScript", logo: "/icons/javascript.svg"},
-        {name: "React", logo: "/icons/react.svg"},
-        {name: "Node.js", logo: "/icons/nodejs.svg"},
+        { name: "HTML", logo: htmlLogo, isLearning: false },
+        { name: "CSS", logo: cssLogo, isLearning: false },
+        { name: "JavaScript", logo: jsLogo, isLearning: false },
+        { name: "React.js", logo: reactLogo, isLearning: true },
+        { name: "Node.js", logo: nodeLogo, isLearning: false },
     ]
     
     return (
         <section className="skills">
-            <h2>Actuellement, j'étudie: <span>{learning.name} {learning.logo}</span></h2>
+            <h2>
+                Actuellement, j'étudie: <br />
+                <span className="skill-learning">
+                    {skills
+                        .filter(skill => skill.isLearning)
+                        .map(skill => (
+                            <>
+                                <img 
+                                    src={skill.logo}
+                                    className="skill-learning-logo" 
+                                />
+                                <span>{skill.name}</span>
+                            </>
+                        ))   
+                    }
+                </span>
+            </h2>
             <div className="carousel">
                 <SkillsCarousel 
                     skills={skills}
