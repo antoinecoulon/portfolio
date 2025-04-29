b3e4acdc9360c25d5ca71266b11cefe221d12087
Antoine COULON <a.antoinecoulon@gmail.com>
2025-04-29 13:13:22 +0200

    Add Chakra UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import './index.css'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
+import { Provider } from '@chakra-ui/react'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
 )
