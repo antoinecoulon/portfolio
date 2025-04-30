5236b9d72a5179ab121a20c9e7e02e4c175aad5a
Antoine COULON <a.antoinecoulon@gmail.com>
2025-04-30 10:05:27 +0200

    Responsive

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,20 +1,71 @@
-import { NavLink } from "react-router"
+import { useState } from "react";
+import { NavLink } from "react-router";
 import { RiHome3Fill } from "react-icons/ri";
-import './Navbar.css'
+import "./Navbar.css";
 
 export default function Navbar() {
-    return (
-        <nav>
-            <ul className='nav-list'>
-                <li><NavLink to={"/"} className={({ isActive }) => isActive ? "active" : ""}>
-                    <RiHome3Fill className="home" />
-                </NavLink></li>
-                <li><NavLink to={"/projects"} className={({ isActive }) => isActive ? "active" : ""}>Projets</NavLink></li>
-                <li><NavLink to={"/skills"} className={({ isActive }) => isActive ? "active" : ""}>Compétences</NavLink></li>
-                <li><NavLink to={"/parcours"} className={({ isActive }) => isActive ? "active" : ""}>Parcours</NavLink></li>
-                <li><NavLink to={"/about"} className={({ isActive }) => isActive ? "active" : ""}>A propos</NavLink></li>
-                <li>Contact</li>
-            </ul>
-        </nav>
-    )
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prevState) => !prevState);
+  };
+
+  return (
+    <nav>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <ul className={`nav-list ${isMenuOpen ? "active" : ""}`}>
+        {isMenuOpen && (
+          <div className="close-menu" onClick={toggleMenu}>
+            <div></div>
+          </div>
+        )}
+
+        <li>
+          <NavLink
+            to={"/"}
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            <RiHome3Fill className="home" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/projects"}
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Projets
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/skills"}
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Compétences
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/parcours"}
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Parcours
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/about"}
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            A propos
+          </NavLink>
+        </li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
 }
